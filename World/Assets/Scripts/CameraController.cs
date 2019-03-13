eb876539581e1b8dcd6fef3b67a24a54662c7f66
Heidi Erwin <heidianneerwin@gmail.com>
2019-03-12 23:57:19 -0400

    first push, initial scene setup

---
+++ World/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start() {
+        offset = transform.position - player.transform.position;
+    }
+
+    // After 'Update()', just before each frame rendered
+    void LateUpdate() {
+        transform.position = player.transform.position + offset;
+    }
+}
