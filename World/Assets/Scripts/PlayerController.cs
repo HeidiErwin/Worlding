cbcc982a4057f7f5f00e4619d1c570e9c1b75118
Heidi Erwin <heidianneerwin@gmail.com>
2019-03-18 23:58:42 -0400

    world setup, dumpling, text

--- World/Assets/Scripts/PlayerController.cs
+++ World/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ using System;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public int mode = 0; // 0 for normal tail, 1 for straight tail
+    public GameObject straightTailMonkey;
+    public GameObject curlyTailMonkey;
 
     private Rigidbody rb;
 
@@ -17,6 +20,20 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) { // roll mode
+            if (mode == 0) {
+                //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                curlyTailMonkey.SetActive(false);
+                straightTailMonkey.SetActive(true);
+                straightTailMonkey.transform.rotation = Quaternion.Euler(90, 0, -90);
+                straightTailMonkey.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + .15f, this.transform.position.z);
+                mode = 1;
+            } else {
+                straightTailMonkey.SetActive(false);
+                curlyTailMonkey.SetActive(true);
+                mode = 0;
+            }
+        }
         MovePlayer();
     }
 
@@ -25,6 +42,11 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
         transform.Translate(movement, Space.Self);
-        //rb.AddForce(movement * speed * 2.0f);
+
+        if (mode == 0) {
+        } else {
+            straightTailMonkey.transform.Rotate(transform.rotation.x, transform.rotation.y + 10.0f, transform.rotation.z);
+            //rb.AddForce(new Vector3(10.0f, 10.0f, 10.0f) * speed * Time.deltaTime);
+        }
     }
 }
---
+++ World/Assets/Scripts/ShowText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour
+{
+    [SerializeField] private GameObject textbox;
+    [SerializeField] private GameObject textboxImage;
+
+    private void OnTriggerStay(Collider other) {
+        textbox.GetComponent<Text>().text = this.GetComponent<Text>().text;
+        textbox.SetActive(true);
+        textboxImage.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        textbox.GetComponent<Text>().text = "";
+        textbox.SetActive(false);
+        textboxImage.SetActive(false);
+    }
+}
