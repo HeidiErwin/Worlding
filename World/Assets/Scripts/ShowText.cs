950d2c02ff0c58ba34aeacb6c905ccab03c3bcd3
Heidi Erwin <heidianneerwin@gmail.com>
2019-03-22 11:04:20 -0400

    tree and house models, adjusting scene

--- World/Assets/Scripts/ShowText.cs
+++ World/Assets/Scripts/ShowText.cs
@@ -8,6 +8,17 @@ public class ShowText : MonoBehaviour
     [SerializeField] private GameObject textbox;
     [SerializeField] private GameObject textboxImage;
 
+    private AudioSource source;
+    public AudioClip foundSound;
+
+    void Awake() {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        source.PlayOneShot(foundSound, 0.5f);
+    }
+
     private void OnTriggerStay(Collider other) {
         textbox.GetComponent<Text>().text = this.GetComponent<Text>().text;
         textbox.SetActive(true);
