ca1fdb1a9ce251aec33a6b33e8f4393358c2dcd3
Heidi Erwin <heidianneerwin@gmail.com>
2019-04-09 11:28:42 -0400

    added grandma, menu

---
+++ World/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void BackToTitle() {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Backyard");
+    }
+
+    public void ShowCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+}
