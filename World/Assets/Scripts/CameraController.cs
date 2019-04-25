4ee07fcaed044673f1c67e013774e5abc6f25307
Heidi Erwin <heidianneerwin@gmail.com>
2019-04-24 22:54:42 -0400

    terrain variety added, organizing grass, bucket added

--- World/Assets/Scripts/CameraController.cs
+++ World/Assets/Scripts/CameraController.cs
@@ -4,59 +4,33 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
     //public GameObject player;
-    private Vector3 offset;
+    [SerializeField] private Vector3 offset; // 0, .4, -1
 
-    float rotationSpeed = 1;
-    public Transform target, player;
-    float mouseX, mouseY;
+    //float rotationSpeed = 1;
+    public Transform target;// player
+    //float mouseX, mouseY;
+    [SerializeField] private float currentZoom; // 1
+    [SerializeField] private float zoomSpeed; // 4
+    [SerializeField] private float minZoom; // 5
+    [SerializeField] private float maxZoom; // 15
 
-    public Transform obstruction;
-    float zoomSpeed = 2f;
+    [SerializeField] private float pitch; //.35
+    [SerializeField] private float yawSpeed = 100f; //.35
 
-    void Start() {
-        obstruction = target;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+    private float currentYaw = 0f; //for camera rotation
+
+    private void Update() {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw += Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
     }
 
     private void LateUpdate() {
-        CamControl();
-        //ViewObstructed();
-    }
+        transform.position = target.position + offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
 
-
-    void CamControl() {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
-
-        transform.LookAt(target);
-
-        if (Input.GetKey(KeyCode.LeftShift)) { // can rotate only camera if hold down left shift
-            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        } else {
-            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-            player.rotation = Quaternion.Euler(0, mouseX, 0);
-        }
-    }
-
-
-    void ViewObstructed() {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, target.position - transform.position, out hit, 4.5f)) {
-            if (hit.collider.gameObject.tag != "Player") {
-                obstruction = hit.transform;
-                obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
-
-                if (Vector3.Distance(obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, target.position) >= 1.5f)
-                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
-            } else {
-                obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-                if (Vector3.Distance(transform.position, target.position) < 4.5f)
-                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
-            }
-        }
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
     }
 
 }
--- World/Assets/Scripts/PlayerController.cs
+++ World/Assets/Scripts/PlayerController.cs
@@ -6,47 +6,62 @@ using UnityEngine.UI;
 using System.Collections;
 using System;
 
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
-    public int mode = 0; // 0 for normal tail, 1 for straight tail
-    public GameObject straightTailMonkey;
-    public GameObject curlyTailMonkey;
+    Camera cam;
 
+    [SerializeField] private LayerMask movementMask; // ensures no hit unless tagged Ground (mask dragged in in inspector)
     private Rigidbody rb;
+    PlayerMotor motor;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
     }
 
-    void FixedUpdate() {
-        if (Input.GetKeyDown(KeyCode.R)) { // roll mode
-            if (mode == 0) {
-                //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-                curlyTailMonkey.SetActive(false);
-                straightTailMonkey.SetActive(true);
-                straightTailMonkey.transform.rotation = Quaternion.Euler(90, 0, -90);
-                straightTailMonkey.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + .15f, this.transform.position.z);
-                mode = 1;
-            } else {
-                straightTailMonkey.SetActive(false);
-                curlyTailMonkey.SetActive(true);
-                mode = 0;
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) { // left mouse button
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask)) {
+                // move our player to what we hit
+                motor.MoveToPoint(hit.point);
+                Debug.Log("ground hit");
             }
         }
-        MovePlayer();
     }
 
-    private void MovePlayer() {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
-        transform.Translate(movement, Space.Self);
+    //void FixedUpdate() {
+    //    if (Input.GetKeyDown(KeyCode.R)) { // roll mode
+    //        if (mode == 0) {
+    //            //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    //            curlyTailMonkey.SetActive(false);
+    //            straightTailMonkey.SetActive(true);
+    //            straightTailMonkey.transform.rotation = Quaternion.Euler(90, 0, -90);
+    //            straightTailMonkey.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + .15f, this.transform.position.z);
+    //            mode = 1;
+    //        } else {
+    //            straightTailMonkey.SetActive(false);
+    //            curlyTailMonkey.SetActive(true);
+    //            mode = 0;
+    //        }
+    //    }
+    //    MovePlayer();
+    //}
 
-        if (mode == 0) {
-        } else {
-            straightTailMonkey.transform.Rotate(transform.rotation.x, transform.rotation.y + 10.0f, transform.rotation.z);
-            //rb.AddForce(new Vector3(10.0f, 10.0f, 10.0f) * speed * Time.deltaTime);
-        }
-    }
+    //private void MovePlayer() {
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
+    //    float moveVertical = Input.GetAxis("Vertical");
+    //    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
+    //    transform.Translate(movement, Space.Self);
+
+    //    if (mode == 0) {
+    //    } else {
+    //        straightTailMonkey.transform.Rotate(transform.rotation.x, transform.rotation.y + 10.0f, transform.rotation.z);
+    //        //rb.AddForce(new Vector3(10.0f, 10.0f, 10.0f) * speed * Time.deltaTime);
+    //    }
+    //}
 }
