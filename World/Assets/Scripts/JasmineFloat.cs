55e989bfcc3b60a5be58c630560dbe61452857f9
Heidi Erwin <heidianneerwin@gmail.com>
2019-04-29 12:26:03 -0400

    added jasmines, floating, more text

---
+++ World/Assets/Scripts/JasmineFloat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JasmineFloat : MonoBehaviour {
+
+    [SerializeField] private float floatSpeed;
+    [SerializeField] private int heightCounter;
+    private float height = .5f;
+    private int counter = 0;
+
+    void Update() {
+        if (counter < heightCounter) {
+            transform.Translate(new Vector3(0, height, 0) * Time.deltaTime * floatSpeed/2);
+            counter++;
+        } else {
+            counter = 0;
+            height *= -1;
+        }
+    }
+}
