191089edcfb26a6f03f7d8a2fa3d3468740ba546
Heidi Erwin <heidianneerwin@gmail.com>
2019-04-28 23:55:09 -0400

    jasmines added, garden layout changed, picnic blankets, more plants, fixed bamboo, dress animated

---
+++ World/Assets/Scripts/JasmineRotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JasmineRotate : MonoBehaviour {
+
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private int heightCounter;
+    private float height = .5f;
+    private int counter = 0;
+
+    void Update() {
+        if (counter < heightCounter) {
+            transform.Translate(new Vector3(0, height, 0) * Time.deltaTime * rotationSpeed);
+            counter++;
+        } else {
+            counter = 0;
+            height *= -1;
+        }
+    }
+}
